ba515cd680854e762c6b005453af0ed083850679
Rijan Maharjan <maharijan127@gmail.com>
2025-11-24 20:06:58 -0500

    Added rps.py file

---
+++ rps.py
@@ -0,0 +1,33 @@
+import random
+
+while True:
+    user_action = input("Enter throw (rock, paper, scissors): ")
+    ai_action = random.choice(["rock", "paper", "scissors"])
+
+    print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+
+    if user_action == ai_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if ai_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if ai_action == "rock":
+            print("Paper covers rock! You win!")
+        elif ai_action == "scissors":
+            print("Scissors cuts paper! You lose.")
+        else:
+            print("Both players selected paper. It's a tie!")
+    elif user_action == "scissors":
+        if ai_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    play_again = input("\nPlay again? (y/n): ").lower()
+
+    if play_again != "y":
+        print("Thanks for playing!")
+        break
